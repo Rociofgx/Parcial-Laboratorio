7bf6bbf587325f66e2caabb5b13da157c310dfb9
Rocío <104164179+Rociofgx@users.noreply.github.com>
2023-11-17 23:02:27 -0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+import pygame
+origen = (0, 0)
+WIDTH = 800
+HEIGHT = 600
+resolucion = WIDTH , HEIGHT 
+
+# Colores
+negro = (0,0,0)
+rojo = (255,0,0)
+blanco = (255,255,255)
+
+is_menu_active = True
+is_pausa = False
+FPS = 60
